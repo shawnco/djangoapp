71bbd5050bca709380a97033c94a28d881807c54
shawnco <shawnc366@gmail.com>
2022-07-19 14:44:29 -0500

    begin some views for the forms

--- starships/urls.py
+++ starships/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'starships'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('new/', views.CreateStarshipView.as_view(), name='create-starship'),
+    path('<int:pk>/', views.StarshipDetailView.as_view(), name='view-starship'),
+    path('<int:pk>/edit/', views.UpdateStarshipView.as_view(), name='update_starship')
 ]
--- starships/views.py
+++ starships/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, reverse
+from django.views.generic import TemplateView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Starship
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class StarshipDetailView(DetailView):
+    model = Starship
+    template_name = 'detail.html'
+
+class CreateStarshipView(CreateView):
+    template_name = 'create.html'
+    model = Starship
+    fields = ['name', 'registry']
+    
+    def get_success_url(self):
+        return reverse('starships:index')
+
+class UpdateStarshipView(UpdateView):
+    template_name = 'update.html'
+    model = Starship
+    fields = ['name', 'registry']
+
+    def get_success_url(self):
+        return reverse('starships:index')
